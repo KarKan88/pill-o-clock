f580c25bf5bda26172c35dd196195e61b72bbaa9
Surya <sr298591@dal.ca>
2022-03-25 12:06:07 -0300

    Setup navigation for next button

--- app/src/main/java/com/example/pilloclock/EnterDetails.kt
+++ app/src/main/java/com/example/pilloclock/EnterDetails.kt
@@ -1,11 +1,19 @@
 package com.example.pilloclock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EnterDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_details)
+
+        var button = findViewById<Button>(R.id.nextButton)
+        button.setOnClickListener {
+            val intent = Intent(this, VerifyDetails::class.java)
+            startActivity(intent)
+        }
     }
 }
