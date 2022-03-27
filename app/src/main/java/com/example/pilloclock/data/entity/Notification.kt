9a05b3a07332c286c714b220611be594989a947e
Karthik Kannan <kr676628@dal.ca>
2022-03-27 13:02:59 -0300

    Notification Fragment and service completed

--- app/src/main/java/com/example/pilloclock/data/entity/Notification.kt
+++ app/src/main/java/com/example/pilloclock/data/entity/Notification.kt
@@ -2,11 +2,14 @@ package com.example.pilloclock.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.pilloclock.constants.NotificationStatus
+import java.sql.Timestamp
 
 @Entity
 data class Notification(
     @PrimaryKey
     val id: String,
     val text: String?,
-    val status: String?
+    val status: NotificationStatus?,
+    val timestamp: String?,
 )
