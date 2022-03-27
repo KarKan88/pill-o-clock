d6c2e4086584c52d7acaa3f3935ecdee2184a4d5
jkim <jn764450@dal.ca>
2022-03-27 18:49:11 -0300

    Edit User Profile UI

---
+++ app/src/main/java/com/example/pilloclock/EditUserProfile.kt
@@ -0,0 +1,16 @@
+package com.example.pilloclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+
+class EditUserProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user_profile)
+    }
+
+}
